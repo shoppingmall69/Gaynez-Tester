399641dad0166271a4b614aec08186eb945f0e55
niggachan <106511768+shoppingmall69@users.noreply.github.com>
2022-08-07 09:46:11 +1000

    Add files via upload

---
+++ Bruh.py
@@ -0,0 +1,8 @@
+#Wagwan,
+#have you ever felt like the online gayness tests aren't accurate, and you need something more accurate?
+#If yes, then this is for you.
+
+if input("What is your gender, write M for male or F for female: ") == input('Are you attracted to males, write M for male or F for female: '):
+        print('You are gay.')
+else:
+    print('Either you are not gay, or you provided incorrect information.')
